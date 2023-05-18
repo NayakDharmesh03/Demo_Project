5717e3ff89249a3e91f4eadc86156bc009e7e439
Dharmesh Nayak <>
2023-05-18 18:49:46 +0530

    comment deleted

--- NewAppProject/DemoFile/ViewController.swift
+++ NewAppProject/DemoFile/ViewController.swift
@@ -7,10 +7,6 @@
 
 
 import UIKit
-/*
- let normalFont = UIFont.boldSystemFont(ofSize: 20)
- let selectedFont = UIFont.boldSystemFont(ofSize: 20)
- */
 
 
 class ViewController: UIViewController {
@@ -18,42 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    //MARK: - new cod added by dharmesh
-         
-         // Set up the segmented control
-      //MARK: - CODE DELETED
- 
-         // Set the font attributes
-         let normalFont = UIFont.systemFont(ofSize: 20, weight: .bold)
-         let selectedFont = UIFont.systemFont(ofSize: 20, weight: .bold)
-         
-         // Set the text color attributes
-         let normalTextAttributes: [NSAttributedString.Key: Any] = [
-             .foregroundColor: UIColor.gray,
-             .font: normalFont
-         ]
-         
-         let selectedTextAttributes: [NSAttributedString.Key: Any] = [
-             .foregroundColor: UIColor.white,
-             .font: selectedFont
-         ]
-         
-         // Apply the text color attributes to the segmented control
-         segmentedControl.setTitleTextAttributes(normalTextAttributes, for: .normal)
-         segmentedControl.setTitleTextAttributes(selectedTextAttributes, for: .selected)
-         
-         // Position the segmented control
-         segmentedControl.frame = CGRect(x: 50, y: 100, width: 300, height: 40)
-     }
      
-     @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-         // Handle segmented control value changes here
-         print("Selected segment index: \(sender.selectedSegmentIndex)")
-     }
- 
-//MARK: -  end code
-
-
+    
     @IBAction func btnMenuBarbuttonItemTapped(_ sender: UIBarButtonItem) {
         let camera = UIAction(title: "Camera", image: UIImage(systemName: "camera")){ _ in
             print("camera tapped")
